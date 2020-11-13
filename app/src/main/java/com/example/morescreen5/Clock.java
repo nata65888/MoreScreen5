f8c2cdc0a07b85183605e91efe7234003a65a022
nata65888 <nata65888@gmail.com>
2020-11-13 12:23:32 +0300

    Практическая рпабота 5

---
+++ app/src/main/java/com/example/morescreen5/Clock.java
@@ -0,0 +1,14 @@
+package com.example.morescreen5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Clock extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clock);
+    }
+}
---
+++ app/src/main/java/com/example/morescreen5/Fon6.java
@@ -0,0 +1,14 @@
+package com.example.morescreen5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Fon6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fon6);
+    }
+}
